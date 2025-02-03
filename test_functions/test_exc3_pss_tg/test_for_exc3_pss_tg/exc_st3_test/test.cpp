e97de03d416476963d7513949221e0f58e815c07
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-02-03 17:53:49 +0330

    test funtion is finished , tests not passed yet

--- test_functions/test_exc3_pss_tg/test_for_exc3_pss_tg/exc_st3_test/test.cpp
+++ test_functions/test_exc3_pss_tg/test_for_exc3_pss_tg/exc_st3_test/test.cpp
@@ -56,14 +56,76 @@ int main()
 
     std::cout << "pelect matrix:\n" << pelect << std::endl;
 
-    
+    Eigen::MatrixXd qelect(4, 1);
+    qelect << 1.27119216215814, 
+              1.94200544624984, 
+              1.13415163866497, 
+              1.76305806835366;
+
+    std::cout << "qelect matrix:\n" << qelect << std::endl;
+
+    Eigen::MatrixXd eterm(4, 1);
+    eterm << 1.01, 1.01, 1.01, 1.01 ;
+
+    std::cout << "eterm matrix:\n" << eterm << std::endl;
+
+    Eigen::MatrixXd theta(14, 1);
+    theta <<  0.322885911618951, 
+              0.134825139650961, 
+             -0.129863257254776, 
+             -0.17857604175599, 
+              0.2020801912147, 
+             -0.157298115763022, 
+             -0.333711889371994, 
+             -0.594510944060937, 
+             -0.681034322781182, 
+              0.0170136892433736, 
+             -0.367460704798512, 
+             -0.273536351796361, 
+             -0.451171213764415, 
+              0;
+
+    std::cout << "theta matrix:\n" << theta << std::endl;    
+
+    Eigen::MatrixXd mac_pot(4, 15);
+    mac_pot << 0.111111111111111, 1, 6.9795, -11.2361, 5.32200000000001, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+               0.111111111111111, 1, 6.9795, -11.2361, 5.32200000000001, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+               0.111111111111111, 1, 6.9795, -11.2361, 5.32200000000001, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+               0.111111111111111, 1, 6.9795, -11.2361, 5.32200000000001, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+
+    std::cout << "mac_pot matrix:\n" << mac_pot << std::endl;
 
 
+    Eigen::MatrixXd fldcur(4, 1);
+    fldcur << 1.87245065830439, 
+              1.95024192439475, 
+              1.81514178352559, 
+              1.91659268284482;
+
+    std::cout << "fldcur matrix:\n" << fldcur << std::endl;
 
 
+    Eigen::MatrixXd st3_TB_idx(4, 1);
+    st3_TB_idx << 1, 2, 3, 4;
 
+    std::cout << "st3_TB_idx matrix:\n" << st3_TB_idx << std::endl;
 
+    int n_st3 = 4;
 
+    MatrixXd Efd , exc_pot , V_B , V_R , V_A , V_As , V_TR , R_f;
+
+    cout << "this is before the function is called\n";
+
+    exc_st3(
+        exc_con, mac_con,mac_pot, bus_int, 
+        mac_int, vex, pelect, 
+        qelect, eterm, theta, 
+        fldcur, Efd, exc_pot, 
+        V_B, V_R , V_A, V_As, 
+        V_TR, R_f, st3_idx, 
+        st3_TB_idx);
+
+    cout << "this is after the function \n";
 
 
 
